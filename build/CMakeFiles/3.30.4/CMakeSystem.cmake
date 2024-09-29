2084da7ce3302d55d1b1ae12cad39c218c29c3b7
noBthd <Egorkir0401@gmail.com>
2024-09-29 23:20:37 +0300

    sfml installed and CMakeLists.txt is configured + project is builded

--- build/CMakeFiles/3.30.4/CMakeSystem.cmake
+++ build/CMakeFiles/3.30.4/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "24.0.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
 
-
+include("/Users/egorkirichenko/.vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Darwin-24.0.0")
 set(CMAKE_SYSTEM_NAME "Darwin")
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}

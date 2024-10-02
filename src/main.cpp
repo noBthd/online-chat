ae78cd54511aaa9c36bf9aa282aa0cd99d8cfb90
noBthd <Egorkir0401@gmail.com>
2024-10-02 22:20:10 +0300

    Added multithreading, some optimization

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
 #include <thread>
 #include <string>
+#include <atomic>
 #include <iostream>
 #include <SFML/Network.hpp>
 
 
 void startServer(sf::TcpSocket& socket);
 void startClient(sf::TcpSocket& socket);
-void reciveMessage(sf::TcpSocket& socket, sf::Packet& packet);
+void reciveMessage(sf::TcpSocket& socket, sf::Packet& packet, std::atomic<bool>& isRunning);
 void sendMessage(std::string& msg, std::string& name, sf::Packet& packet, sf::TcpSocket& socket);
 
 int main() {
     sf::IpAddress ip = sf::IpAddress::getLocalAddress();
     sf::TcpSocket socket;
+    sf::Packet packet;
 
     char type;
     std::cout << "[s] - server\n[c] - client\n";
     std::cin >> type;
 
-    if(type == 's') 
-    {
+    if(type == 's') {
         startServer(socket);
     } 
-    else if (type == 'c') 
-    {
+    else if (type == 'c') {
         startClient(socket);
     }
 
@@ -32,34 +32,23 @@ int main() {
 
     socket.setBlocking(false);
 
-    std::string msg = "";
-    sf::Packet packet;
+    std::atomic<bool> isRunning(true);
 
-    while (true)
-    {
+    std::thread t1(reciveMessage, std::ref(socket), std::ref(packet), std::ref(isRunning));
+
+    std::string msg = "";
+    while (isRunning) {
         std::cout << "msg: ";
         std::cin >> msg;
 
+        if(msg == "/exit") 
+            break;
+
         sendMessage(msg, name, packet, socket);
-        // if(msg != "")
-        // {
-        //     packet.clear();
-        //     packet << name << msg;
-
-        //     socket.send(packet);
-        //     msg = "";
-        // }
-
-        reciveMessage(socket, packet);
-        // if(socket.receive(packet) == sf::Socket::Done)
-        // {
-        //     std::string nameRec;
-        //     std::string msgRec;
-
-        //     packet >> nameRec >> msgRec;
-        //     std::cout << nameRec << ": " << msgRec << "\n";
-        // }
     }
+
+    t1.join();
+
     return 0;
 }
 
@@ -67,39 +56,41 @@ void startServer(sf::TcpSocket& socket) {
     sf::TcpListener server;
     server.listen(2000);
 
-    if(server.accept(socket) != sf::Socket::Done) 
-    {
+    if(server.accept(socket) != sf::Socket::Done) {
         std::cout << "Error: server cannot accept socket\n";
     }
 }
-
+ 
 void startClient(sf::TcpSocket& socket) {
     sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 
-    if(socket.connect(ip, 2000) != sf::Socket::Done) 
-    {
+    if(socket.connect(ip, 2000) != sf::Socket::Done) {
         std::cout << "Error: client cannto connect to the socket\n";
     }
 }
 
-void reciveMessage(sf::TcpSocket& socket, sf::Packet& packet) {
-    if(socket.receive(packet) == sf::Socket::Done)
-    {
-        std::string nameRec;
-        std::string msgRec;
+void reciveMessage(sf::TcpSocket& socket, sf::Packet& packet, std::atomic<bool>& isRunning) {
+    while(isRunning) {
+        if(socket.receive(packet) == sf::Socket::Done) {
+            std::string nameRec;
+            std::string msgRec;
+            packet >> nameRec >> msgRec;
 
-        packet >> nameRec >> msgRec;
-        std::cout << nameRec << ": " << msgRec << "\n";
+            std::cout << nameRec << ": " << msgRec << "\n" << "msg: ";
+            std::cout.flush();
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
 
 void sendMessage(std::string& msg, std::string& name, sf::Packet& packet, sf::TcpSocket& socket) {
-    if(msg != "")
-    {
+    if(msg != "") {
         packet.clear();
         packet << name << msg;
-        socket.send(packet);
-
+        if(socket.send(packet) != sf::Socket::Done) {
+            std::cout << "Error sending message\n";
+        }
         msg = "";
     }
 }
